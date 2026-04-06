8c8184c2c780dee113311b531b2a9a055fcef331
carlo-caringi <ccaringi@hotmail.com>
2026-04-06 23:59:27 +0200

    Included integration test conditions in Home.test.js

--- frontend/src/pages/Home/Home.test.js
+++ frontend/src/pages/Home/Home.test.js
@@ -4,7 +4,10 @@ import axios from 'axios';
 import Home from '.';
 
 describe('Test Home', () => {
-  test('Test Render', async () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  test('(Functional) Test Render', async () => {
     //Arrange: Setup the mock API
     //Listen for any GET requests using the axios module
     const mockGet = jest.spyOn(axios, 'get');
@@ -47,4 +50,16 @@ describe('Test Home', () => {
     //The word Appeateasers should be in there as defined in the mock response above.
     expect(await screen.findByText('Appeteasers')).toBeInTheDocument();
   });
+
+  test('(Integration) Test Render', async () => {
+    //Act: Call the Home page
+    render(<Home />);
+
+    //Assert: Check the values in the rendered Home page.
+    //There should be 2 categories as defined in the mock response above
+    expect(await screen.findAllByTestId(/category-item/i)).toHaveLength(2);
+    //The word Appeateasers should be in there as defined in the mock response above.
+    expect(await screen.findByText('Appeteasers')).toBeInTheDocument();
+  });
+
 });
